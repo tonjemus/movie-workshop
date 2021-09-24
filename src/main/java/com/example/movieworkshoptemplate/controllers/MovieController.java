552027b5a2f8d74bcd5bccac0b05a58e512018e4
nicklasdean <61466422+nicklasdean@users.noreply.github.com>
2021-09-24 11:23:12 +0200

    First

---
+++ src/main/java/com/example/movieworkshoptemplate/controllers/MovieController.java
@@ -0,0 +1,8 @@
+package com.example.movieworkshoptemplate.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieController {
+    //Controller methods
+}
